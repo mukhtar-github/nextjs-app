073d8948eccbe9abd98f1a56bcae41b87946b7b3
Mukhtar <mukhtartanimugarba@gmail.com>
2022-04-08 15:45:21 +0100

    Update changes

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Head from 'next/head'
 
 export default function Home() {
@@ -10,9 +11,12 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read{' '}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
         </h1>
-
+        
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
