0cc87df342ea9e4fae4d9bcbca6f711cc0fa5976
Mukhtar <mukhtartanimugarba@gmail.com>
2022-04-08 16:34:12 +0100

    Update changes

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,10 +1,10 @@
-import Link from 'next/link'
 import Head from 'next/head'
-import Script from 'next/script'
+import Link from 'next/link'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -14,6 +14,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
 }
