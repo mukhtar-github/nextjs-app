4f43dba9d7fb8a95fecda6ac8c9e729f2ec1dc68
Mukhtar <mukhtartanimugarba@gmail.com>
2022-04-09 09:53:41 +0100

    Update file

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
@@ -22,13 +23,15 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <Head>
+            <title>{postData.title}</title>
+        </Head>
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
