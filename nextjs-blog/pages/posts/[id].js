bd25d26e3983174da91478f6e18e10ce46fc8d08
Mukhtar <mukhtartanimugarba@gmail.com>
2022-04-09 10:07:06 +0100

    Update file

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -2,6 +2,7 @@ import Date from '../../components/date'
 import Head from 'next/head'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticProps({ params }) {
   // Add the "await" keyword like this:
@@ -24,16 +25,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-        <Head>
-            <title>{postData.title}</title>
-        </Head>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        <Date dateString={postData.date} />
-        <br />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
