28da189a330cbed3925c38855c4e6bf91962709d
Mukhtar <mukhtartanimugarba@gmail.com>
2022-04-09 09:45:01 +0100

    Update files

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -2,7 +2,8 @@ import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -26,6 +27,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
