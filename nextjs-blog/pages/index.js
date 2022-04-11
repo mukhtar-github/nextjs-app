8a2ad2ce3306dace76664814dc0e9c3922242883
Mukhtar <mukhtartanimugarba@gmail.com>
2022-04-11 12:10:34 +0100

    Update file

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -23,7 +23,7 @@ export default function Home ({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <div>
           <p>
-            Hi wave! I am <strong>Mukhtar</strong>, a Mechanical Engineer transforming into a Software Developer. I'm a Fleet Manager at the Transport Section of the Federal High Court of Nigeria. I started learning programming in October 2018, and so far, the time metrics in my programming journey have amounted to about 2500 hours on counting.<br />
+            Hi! I am <strong>Mukhtar</strong>, a Mechanical Engineer transforming into a Software Developer. I'm a Fleet Manager at the Transport Section of the Federal High Court of Nigeria. I started learning programming in October 2018, and so far, the time metrics in my programming journey have amounted to about 2500 hours on counting.<br />
             I'm currently working on a SaaS Project named <strong>VIMAP</strong> which is an acronym for <i>Vehicles Information Management Application</i>. The idea of this project came as a result of finding a solution for the replacement of hard copies of the vehicle's information with an application for easy access and manipulation. The software's features are the Collection and Storage of Vehicle Information, Inventory, GPS tracking, Maps, Insurance updates, e.t.c. So far, I was able to build a tested development backend using Node.js, and I'm presently ongoing with some React courses to help me build a friendly interacting UI. <strong>VIMAP</strong> is a Startup project, so wish us Goodluck, please.<br /> Thank you.
           </p>
         </div>
