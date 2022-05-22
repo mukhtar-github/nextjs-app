1ee9ccb71610562f03a98aaeeffff02d87ede2ba
Mukhtartg <mukhtartanimugarba@gmail.com>
2022-05-22 09:14:21 +0100

    Update index.js

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -24,10 +24,8 @@ export default function Home ({ allPostsData }) {
         <div>
           <p>
         Hi! I am <strong>Mukhtar</strong>, a Mechanical Engineer transformed into a Software Developer. I started my programming journey in October 2018 as a non-traditional developer with no background in CS. So far, the time metrics have amounted to about 2500 hours on counting.<br />
-As a Fleet Engineer at the Organization where I work, the challenge of going through archives to get vehicle information for updates was time-consuming. As a result of that challenge, the idea of <strong><a href="https://www.vimap.io/">VIMAP</a></strong>, an acronym for <i>Vehicles Information Management Application</i>, came up to provide a solution for the replacement of hard copies of vehicle information and easy accessing and updating of vehicle information. 
-The software's features are; but are not limited to the Collection and Storage of Vehicles Information, Inventory, GPS tracking, and Insurance updates. <strong>VIMAP</strong> is a Startup project, so wish us Goodluck, please. Thank you.
-          </p>
-        </div>
+As a Fleet Manager at the Organization where I work, the challenge of going through archives to get vehicle information for updates was time-consuming. As a result of that challenge, the idea of <strong><a href="https://www.vimap.io/">VIMAP</a></strong>, an acronym for <i>Vehicles Information Management Application</i>, came up to provide a solution for the replacement of hard copies of vehicle information and easy accessing and updating of vehicle information. 
+The software's features are; but are not limited to the Collection and Storage of Vehicles Information, Inventory, GPS tracking, and Insurance updates.
         <a href="https://twitter.com/garba_tanimu" class="twitter-follow-button" data-size="large" data-show-screen-name="false" data-show-count="false">@garba_tanimu</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>&nbsp;&nbsp;
         <a class="github-button" href="https://github.com/mukhtar-github" data-size="large" aria-label="Follow @mukhtar-github on GitHub">GitHub</a>
       </section>
