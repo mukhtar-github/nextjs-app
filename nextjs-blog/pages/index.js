1c5cb142825fe2ed85362881c61aaa28f980320a
Mukhtartg <mukhtartanimugarba@gmail.com>
2022-05-22 07:47:55 +0100

    Update index.js

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -23,10 +23,9 @@ export default function Home ({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <div>
           <p>
-            Hi! I am <strong>Mukhtar</strong>, a Mechanical Engineer transformed into a Software Developer. I started my programming journey in October 2018, as a non-traditional developer with no background in CS. So far the time metrics have amounted to about 2500 hours on counting.<br />
-Being a Fleet Engineer at the Organization where I work, we were always faced with the challange of going through archives to get vehicles information and to update the information as well. 
-As a result of that challenge, the idea of <strong><a href="https://www.vimap.io/">VIMAP</a></strong> which is an acronym for <i>Vehicles Information Management Application</i> came up to provide solution for the replacement of hard copies of vehicles information, and for easy accessing and updating vehicles information. 
-The software's features are, but not limited to, the Collection and Storage of Vehicles Information, Inventory, GPS tracking, Maps, Insurance updates. <strong>VIMAP</strong> is a Startup project, so wish us Goodluck, please. Thank you.
+        Hi! I am <strong>Mukhtar</strong> a Mechanical Engineer transformed into a Software Developer. I started my programming journey in October 2018 as a non-traditional developer with no background in CS. So far, the time metrics have amounted to about 2500 hours on counting.<br />
+As a Fleet Engineer at the Organization where I work, the challenge of going through archives to get vehicle information for updates was time-consuming. As a result of that challenge, the idea of <strong><a href="https://www.vimap.io/">VIMAP</a></strong>, an acronym for <i>Vehicles Information Management Application</i>, came up to provide a solution for the replacement of hard copies of vehicle information and easy accessing and updating of vehicle information. The software's features are; but are not limited to the Collection and Storage of Vehicles Information, Inventory, GPS tracking, and Insurance updates. VIMAP is a Startup project, so wish us Goodluck, please. Thank you.
+            Hi! I am , a Mechanical Engineer transformed into a Software Developer. I started my programming journey in October 2018, as a non-traditional developer with no background in CS. So far the time metrics have amounted to about 2500 hours on counting.
           </p>
         </div>
         <a href="https://twitter.com/garba_tanimu" class="twitter-follow-button" data-size="large" data-show-screen-name="false" data-show-count="false">@garba_tanimu</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>&nbsp;&nbsp;
